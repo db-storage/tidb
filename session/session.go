41f7fc105b75314482e1eea2e2e9032a90d5332a
db-storage <donghuanqing@yahoo.com>
2019-05-14 17:47:20 +0800

    change HintName.L to HintName.O

--- session/session.go
+++ session/session.go
@@ -977,7 +977,7 @@ type exeutionHints struct {
 func (s *session) getExecutionHints(hints []*ast.TableOptimizerHint) (execHints *exeutionHints) {
 	execHints = &exeutionHints{maxExecutionTime: invalidExecutionTime}
 	for _, hint := range hints {
-		switch hint.HintName.L {
+		switch hint.HintName.O {
 		case TiDBMaxExecutionTime:
 			execHints.maxExecutionTime = hint.MaxExecutionTime
 		default:
